9c71dfa179e4cc8e34001a9cf673b7327c2ecbfb
Aleksei da Silva <Alekseidasilva@hotmail.com>
2024-02-27 17:57:20 +0100

    FEAT: Adicionado Projecto de Teste Novamente

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension.Tests;
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShoulConvertDeciamlToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+        Assert.AreEqual(27998, cents);
+    }
+}
